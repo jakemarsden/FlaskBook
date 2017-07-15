dcb18d99fc52a1f92610b370fb144aac80e7d07d
jakemarsden <jakemarsdenjm@gmail.com>
2017-07-16 02:06:41 +1200

    Show story fulltext view at "/stories/<int:story_id>"

--- flaskbook/ui/views.py
+++ flaskbook/ui/views.py
@@ -1,8 +1,30 @@
+from sqlalchemy.sql import collate
+
 from flaskbook import app
-from flaskbook.ui.ui import Page, render
+from flaskbook.orm.models import Category, Story
+from flaskbook.ui.ui import MenuItem, Page, render
 
 
 @app.route('/')
 @app.route('/index')
 def get_index():
     return render('index.html', Page())
+
+
+@app.route('/stories/<int:story_id>')
+def get_story_by_id(story_id: int):
+    story = (Story.query
+             .filter(Story.id == story_id)
+             .one_or_none())
+    if story is None:
+        raise RuntimeError
+    else:
+        return render('story_fulltext.html', _stories_page(), story=story)
+
+
+def _stories_page():
+    categories = (Category.query
+                  .order_by(collate(Category.name, 'NOCASE'))
+                  .all())
+    menu_items = [MenuItem(category.name, '/category/%r' % category.id) for category in categories]
+    return Page(menu_items)
