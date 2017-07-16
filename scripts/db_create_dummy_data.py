2886c77d40bf9b68d293786485c3f7de53429f75
jakemarsden <jakemarsdenjm@gmail.com>
2017-07-16 20:45:17 +1200

    Format dummy data script

--- scripts/db_create_dummy_data.py
+++ scripts/db_create_dummy_data.py
@@ -2,6 +2,7 @@
 from typing import Optional
 
 import requests
+from sqlalchemy.orm.properties import RelationshipProperty
 from sqlalchemy_imageattach.context import store_context
 
 from flaskbook import db, image_store
@@ -22,40 +23,30 @@ def _drop_all_data():
 
 
 def _create_dummy_category(template: dict) -> Category:
-    category = Category()
-    category.name = template['name']
+    category = Category(name=template['name'])
     db.session.add(category)
     db.session.commit()
     return category
 
 
 def _create_dummy_user(template: dict) -> User:
-    user = User()
-    user.nickname = template['name']
+    user = User(nickname=template['name'])
     with store_context(image_store):
-        if template['avatar'] is not None:
-            response = _request(template['avatar'])
-            if response is not None:
-                user.avatar.from_blob(response.content)
+        _set_image_field_from_url(template['avatar'], user.avatar)
         db.session.add(user)
         db.session.commit()
     return user
 
 
 def _create_dummy_album(template: dict) -> Album:
-    album = Album()
-    album.title = template['title']
-    album.author = template['author']
-    album.category = template['category']
+    album = Album(title=template['title'],
+                  author=template['author'],
+                  category=template['category'])
     with store_context(image_store):
         for idx, entry_template in enumerate(template['entries']):
-            entry = AlbumEntry()
-            entry.order = (idx + 1)
-            entry.caption = entry_template['caption']
-            if entry_template['image'] is not None:
-                response = _request(entry_template['image'])
-                if response is not None:
-                    entry.image.from_blob(response.content)
+            entry = AlbumEntry(order=(idx + 1),
+                               caption=entry_template['caption'])
+            _set_image_field_from_url(entry_template['image'], entry.image)
             album.entries.append(entry)
         db.session.add(album)
         db.session.commit()
@@ -63,20 +54,23 @@ def _create_dummy_album(template: dict) -> Album:
 
 
 def _create_dummy_story(template: dict) -> Story:
-    story = Story()
-    story.title = template['title']
-    story.flavour_text = template['flavour']
-    story.fulltext_html = template['fulltext']
-    story.author = template['author']
-    story.category = template['category']
+    story = Story(title=template['title'],
+                  flavour_text=template['flavour'],
+                  fulltext_html=template['fulltext'],
+                  author=template['author'],
+                  category=template['category'])
     with store_context(image_store):
-        if template['cover'] is not None:
-            response = _request(template['cover'])
-            if response is not None:
-                story.cover.from_blob(response.content)
+        _set_image_field_from_url(template['cover'], story.cover)
         db.session.add(story)
         db.session.commit()
     return story
+
+
+def _set_image_field_from_url(url: Optional[str], field: RelationshipProperty):
+    if url is not None:
+        response = _request(url)
+        if response is not None:
+            field.from_blob(response.content)
 
 
 def _lorem_ipsum(paras: int, as_html: bool = True) -> Optional[str]:
@@ -106,107 +100,87 @@ _categories = [{'name': 'Test 1'},
                {'name': 'This test category has an extra long name for testing purposes'}]
 _dummy_categories = [_create_dummy_category(category) for category in _categories]
 
-_users = [{
-    'name': 'Test 1',
-    'avatar': None
-}, {
-    'name': 'Test 2',
-    'avatar': None
-}, {
-    'name': 'Test 3',
-    'avatar': None
-}, {
-    'name': 'This test user has an extra long name for testing purposes',
-    'avatar': 'http://www.gravatar.com/avatar/?d=mm'
-}]
+_users = [{'name': 'Test 1', 'avatar': None},
+          {'name': 'Test 2', 'avatar': None},
+          {'name': 'Test 3', 'avatar': None},
+          {'name': 'This test user has an extra long name for testing purposes',
+           'avatar': 'http://www.gravatar.com/avatar/?d=mm'}]
 _dummy_users = [_create_dummy_user(user) for user in _users]
 
-_albums = [{
-    'title': 'Test Album 1',
-    'author': _dummy_users[0],
-    'category': _dummy_categories[0],
-    'entries': [{'caption': 'Test 1', 'image': 'https://upload.wikimedia.org/wikipedia/en/7/7d/Bliss.png'},
-                {'caption': 'Test 2', 'image': None},
-                {'caption': 'Test 3', 'image': None}]
-}, {
-    'title': 'Mostly Empty',
-    'author': None,
-    'category': None,
-    'entries': []
-}, {
-    'title': None,
-    'author': None,
-    'category': None,
-    'entries': []
-}, {
-    'title': 'This test album has a very long title and contains lots of images with very long captions and was '
-             'written by an author with a very long nickname and is part of a category which has a very long name in '
-             'order to allow for more thorough dev testing',
-    'author': _dummy_users[3],
-    'category': _dummy_categories[3],
-    'entries': [{'caption': _lorem_ipsum(1, as_html=False),
-                 'image': 'https://upload.wikimedia.org/wikipedia/en/7/7d/Bliss.png'},
-                {'caption': _lorem_ipsum(1, as_html=False), 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': _lorem_ipsum(1, as_html=False), 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None},
-                {'caption': None, 'image': None}]
-}]
+_albums = [{'title': 'Test Album 1',
+            'author': _dummy_users[0],
+            'category': _dummy_categories[0],
+            'entries': [{'caption': 'Test 1', 'image': 'https://upload.wikimedia.org/wikipedia/en/7/7d/Bliss.png'},
+                        {'caption': 'Test 2', 'image': None},
+                        {'caption': 'Test 3', 'image': None}]},
+           {'title': 'Mostly Empty',
+            'author': None,
+            'category': None,
+            'entries': []},
+           {'title': None,
+            'author': None,
+            'category': None,
+            'entries': []},
+           {'title': 'This test album has a very long title and contains lots of images with very long captions and '
+                     'was written by an author with a very long nickname and is part of a category which has a very '
+                     'long name in order to allow for more thorough dev testing',
+            'author': _dummy_users[3],
+            'category': _dummy_categories[3],
+            'entries': [{'caption': _lorem_ipsum(1, as_html=False),
+                         'image': 'https://upload.wikimedia.org/wikipedia/en/7/7d/Bliss.png'},
+                        {'caption': _lorem_ipsum(1, as_html=False), 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': _lorem_ipsum(1, as_html=False), 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None},
+                        {'caption': None, 'image': None}]}]
 _dummy_albums = [_create_dummy_album(album) for album in _albums]
 
-_stories = [{
-    'title': 'Test Story 1',
-    'flavour': 'This test story is vanilla flavoured.',
-    'fulltext': _lorem_ipsum(3),
-    'author': _dummy_users[0],
-    'category': _dummy_categories[0],
-    'cover': None
-}, {
-    'title': 'Test Story 2',
-    'flavour': 'This test story is chocolate flavoured.',
-    'fulltext': _lorem_ipsum(3),
-    'author': _dummy_users[0],
-    'category': _dummy_categories[0],
-    'cover': None
-}, {
-    'title': 'Test Story 3',
-    'flavour': 'This test story is strawberry flavoured.',
-    'fulltext': _lorem_ipsum(3),
-    'author': _dummy_users[1],
-    'category': _dummy_categories[1],
-    'cover': None
-}, {
-    'title': None,
-    'flavour': None,
-    'fulltext': None,
-    'author': None,
-    'category': None,
-    'cover': None
-}, {
-    'title': 'Mostly Empty',
-    'flavour': 'This story is empty except for the title and the flavour text.',
-    'fulltext': None,
-    'author': None,
-    'category': None,
-    'cover': None
-}, {
-    'title': 'This test story has a very long title and some very long flavour text and was written by an author with '
-             'a very long nickname and is part of a category which has a very long name in order to allow for more '
-             'thorough dev testing',
-    'flavour': _lorem_ipsum(1, as_html=False),
-    'fulltext': _lorem_ipsum(20),
-    'author': _dummy_users[3],
-    'category': _dummy_categories[3],
-    'cover': 'https://upload.wikimedia.org/wikipedia/en/7/7d/Bliss.png'
-}]
+_stories = [{'title': 'Test Story 1',
+             'flavour': 'This test story is vanilla flavoured.',
+             'fulltext': _lorem_ipsum(3),
+             'author': _dummy_users[0],
+             'category': _dummy_categories[0],
+             'cover': None},
+            {'title': 'Test Story 2',
+             'flavour': 'This test story is chocolate flavoured.',
+             'fulltext': _lorem_ipsum(3),
+             'author': _dummy_users[0],
+             'category': _dummy_categories[0],
+             'cover': None},
+            {'title': 'Test Story 3',
+             'flavour': 'This test story is strawberry flavoured.',
+             'fulltext': _lorem_ipsum(3),
+             'author': _dummy_users[1],
+             'category': _dummy_categories[1],
+             'cover': None},
+            {'title': None,
+             'flavour': None,
+             'fulltext': None,
+             'author': None,
+             'category': None,
+             'cover': None},
+            {'title': 'Mostly Empty',
+             'flavour': 'This story is empty except for the title and the flavour text.',
+             'fulltext': None,
+             'author': None,
+             'category': None,
+             'cover': None},
+            {'title': 'This test story has a very long title and some very long flavour text and was written by an '
+                      'author with a very long nickname and is part of a category which has a very long name in order '
+                      'to allow for more thorough dev testing',
+             'flavour': _lorem_ipsum(1, as_html=False),
+             'fulltext': _lorem_ipsum(20),
+             'author': _dummy_users[3],
+             'category': _dummy_categories[3],
+             'cover': 'https://upload.wikimedia.org/wikipedia/en/7/7d/Bliss.png'}]
 _dummy_stories = [_create_dummy_story(story) for story in _stories]
