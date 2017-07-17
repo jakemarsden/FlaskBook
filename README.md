7e35a3807e2f1fd8b91bfc296e7a9347113720e8
jakemarsden <jakemarsdenjm@gmail.com>
2017-07-17 23:25:23 +1200

    README, LICENSE and more thorough setup.py

---
+++ README.md
@@ -0,0 +1,7 @@
+# FlaskBook
+
+This is a Python web application designed to let users archive and view media (stories, images etc.) from a variety of
+sources (eg. [Reddit](https://reddit.com) and [Imgur](https://imgur.com)).
+
+This is my first foray into a Python project and is meant to act as a personal learning experience. There is no
+particular practical application I'm working towards.
--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
+import os
+
 from setuptools import setup
+
+
+def read(file_name):
+    base_dir = os.path.dirname(__file__)
+    file_path = os.path.join(base_dir, file_name)
+    return open(file_path, 'rt').read()
+
 
 setup(
     name='FlaskBook',
+    version='0.0.1',
+    author='Jake Marsden',
+    author_email='jakemarsdenjm@gmail.com',
+    url='https://github.com/jakemarsden/FlaskBook',
+    long_description=read('README.md'),
+    license='GNU GPLv3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+
     packages=['flaskbook'],
-    include_package_data=True,
     install_requires=[
         'flask',
         'flask-sqlalchemy',
